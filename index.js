bdc03676ce048154417d64f7c7c8e6927390487c
Ajeya5999 <ajeyasengar@gmail.com>
2023-06-29 23:37:59 +0530

    second commit: cleaning up the code

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
+// Alarm Variables
+
+const alarmTimes = [];
+
+// Alarm Function
+
+setInterval(() => {
+    const date = new Date(), hours = date.getHours(), minutes = date.getMinutes();
+    let hoursString = (hours < 10) ? "0" + hours : hours, minutesString = (minutes < 10) ? "0" + minutes : minutes;
+    let currTime = `${hoursString}:${minutesString}`;
+    let time = alarmTimes.find((timeObject) => {return timeObject.time === currTime});
+    if(time && !(time.hasrung)) {
+        alert("Alarm is ringing");
+        time.hasrung = true;
+    }
+}, 1000);
+
 // Getting the Alarm Time
 
 const alarmTime = document.getElementById("time");
 const setAlarm = document.getElementById("set-alarm");
-let date = new Date();
-let hasrung = false;
-
-// Storage to set alarm times
-
-const alarmTimes = [];
 
 // Adding the alarm time
 
 setAlarm.addEventListener('click', function(event) {
     event.preventDefault();
     alarmTimes.push({
-        time: alarmTime.value
+        time: alarmTime.value,
+        hasrung: false
     });
     alert("alarm is set");
 });
-
-//Ringing the Alarm 
-
-setInterval(function() {
-    date = new Date();
-    let hours = date.getHours(), minutes = date.getMinutes();
-    if(hours < 10) hours = "0" + hours;
-    if(minutes < 10) minutes = "0" + minutes;
-    if(date.getSeconds === 0) hasrung = false;
-    let currTime = `${hours}:${minutes}`;
-    let time = alarmTimes.find(function(timeObj) {
-        return timeObj.time === currTime;
-    })
-    if(time && !(hasrung)) {
-        alert("Alarm is ringing");
-        hasrung = true;
-    }
-}, 1000);
