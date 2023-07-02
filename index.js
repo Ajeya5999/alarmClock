14ac38585c5e5abe07f4ae4d95823bb6917ab39b
Ajeya5999 <ajeyasengar@gmail.com>
2023-07-02 21:20:01 +0530

    fourth commit: functionality that shows the set alarms have been added, along with some cleaner UI

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Alarm Variables
 
 const alarmTimes = [];
+const alarmsContainer = document.getElementById('alarms-container');
 
 // Alarm Function
 
@@ -26,6 +27,35 @@ const setAlarm = document.getElementById("set-alarm");
 
 // Adding the alarm time
 
+function time24to12 (timeString24) {
+    let hours = timeString24.substring(0, timeString24.indexOf(":"));
+    let meridian = (hours >= 12) ? "pm" : "am" ;
+    if(hours > 12) hours -= 12;
+    return `${hours}:${timeString24.substring(timeString24.indexOf(":") + 1, timeString24.length)} ${meridian}`;
+}
+
+function dayNumberToName (selectedDays) {
+    let dayTable = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let days = [];
+    for(let iterator = 0; iterator < selectedDays.length; iterator++) days.push(dayTable[selectedDays[iterator]]);
+    return days;
+}
+
+function addToAlarmList(alarmTime, selectedDays) {
+    let alarmContainer = document.createElement("div");
+    alarmContainer.setAttribute('class', 'alarm-container');
+    let time = time24to12(alarmTime.value);
+    let days = dayNumberToName(selectedDays);
+    let alarmContainerTime = document.createElement("span");
+    alarmContainerTime.setAttribute('class', 'alarm-container-time');
+    let alarmContainerDays = document.createElement("div");
+    alarmContainerDays.setAttribute('class', 'alarm-container-days');
+    alarmContainerTime.innerText = time;
+    alarmContainerDays.innerText = days;
+    alarmContainer.append(alarmContainerTime, alarmContainerDays);
+    alarmsContainer.append(alarmContainer);
+}
+
 setAlarm.addEventListener('click', function(event) {
     event.preventDefault();
     let selectedDays = [];
@@ -37,5 +67,5 @@ setAlarm.addEventListener('click', function(event) {
         hasrung: false,
         days: selectedDays
     });
-    alert("alarm is set");
+    addToAlarmList(alarmTime, selectedDays);
 });
